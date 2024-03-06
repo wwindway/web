b4590a266453a763e36e9b83f54a7968279adabb
wwindway <99008488+wwindway@users.noreply.github.com>
2024-03-06 10:51:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$('#fullpage').fullpage({
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation:true,
+    menu:'.nav',
+    anchors:['1page', '2page','3page','4page','5page']
+});
