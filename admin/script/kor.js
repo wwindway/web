3b9adc41b0162cf236648ff7f379231d8cf55262
PC01\702-00 <aaa@aaa.com>
2025-11-18 15:38:26 +0900

    국영수 프로그램 추가

--- admin/script/kor.js
+++ admin/script/kor.js
@@ -39,4 +39,71 @@ $("#btn").click(() => {
   let view = `<li><p>${num}</p><p>${username.val()}</p><p>${kor.val()}</p><p>${eng.val()}</p><p>${math.val()}</p><p>${total}</p><p>${avg}</p></li>`;
   $(".list ul").append(view);
   num++;
+
+  gradeData.push({
+    username: username.val(),
+    kor: Number(kor.val()),
+    eng: Number(eng.val()),
+    math: Number(math.val()),
+    total: total,
+    avg: avg,
+  });
+
+  username.val("");
+  kor.val("");
+  eng.val("");
+  math.val("");
 });
+
+$(".chartlist").click(function () {
+  $(".chart").css("display", "block");
+  drawChart();
+});
+
+let ctx = document.querySelector(".chartView").getContext("2d");
+
+function drawChart() {
+  let unit = 120;
+  ctx.fillStyle = "#ddd";
+  ctx.fillRect(0, unit, 800, 1);
+  ctx.fillRect(0, unit + 60, 800, 1);
+  unit += 60;
+  ctx.fillRect(0, unit + 60, 800, 1);
+  unit += 60;
+  ctx.fillRect(0, unit + 60, 800, 1);
+  unit += 60;
+  ctx.fillRect(0, unit + 60, 800, 1);
+
+  ctx.fillStyle = "black";
+  ctx.font = "20px Noto Sans KR";
+  let chartLine = 60;
+  ctx.fillText("0", 0, 400 - chartLine);
+  chartLine += 60;
+  ctx.fillText("30", 0, 400 - chartLine);
+  chartLine += 60;
+  ctx.fillText("50", 0, 400 - chartLine);
+  chartLine += 60;
+  ctx.fillText("80", 0, 400 - chartLine);
+  chartLine += 60;
+  ctx.fillText("100", 0, 400 - chartLine);
+
+  let startX = 100; // 첫학생 막대시작위치
+  let gap = 130; // 학생간격
+
+  gradeData.forEach((stu, index) => {
+    let baseY = 360;
+    //  국어
+    ctx.fillStyle = "red";
+    ctx.fillRect(startX, baseY - stu.kor * 3, 30, stu.kor * 3);
+    // 영어
+    ctx.fillStyle = "blue";
+    ctx.fillRect(startX + 35, baseY - stu.eng * 3, 30, stu.eng * 3);
+    // 수학
+    ctx.fillStyle = "green";
+    ctx.fillRect(startX + 70, baseY - stu.math * 3, 30, stu.math * 3);
+    //  이름
+    ctx.fillStyle = "black";
+    ctx.fillText(stu.username, startX + 5, baseY + 30);
+    startX += gap;
+  });
+}
